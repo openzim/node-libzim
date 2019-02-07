8ee5bf27e49cdecf860072d7b185af4540db51c2
Joe Reeve <joseph.reeve@googlemail.com>
2019-02-07 17:05:06 +0000

    Build fixes

--- build/native/binding.gyp
+++ build/native/binding.gyp
@@ -1,14 +1,22 @@
 {
 	"variables": {
-      "libzim": "-L../../../download/",
+      "libzim_dir": "<(module_root_dir)/../../download",
+	  "libzim_include": "../../download/include",
   	},
 	"targets": [
 		{
+			"libraries": [ 
+				"-Wl,-rpath,<(libzim_dir)",
+				"-L<(libzim_dir)",
+				"-lzim",
+			],
+			"include_dirs": [
+				"<(libzim_include)",
+			],
 			"includes": [
 				"../auto.gypi",
 				"../../sources.gypi"
 			],
-			"libraries": [ "<(libzim)", "-lzim" ],
 		}
 	],
 	"includes": [
--- download-libzim.js
+++ download-libzim.js
@@ -3,43 +3,49 @@ const axios = require('axios');
 const mkdirp = require('mkdirp');
 const exec = require('exec-then');
 const fs = require('fs');
+const urlParser = require('url');
 
 mkdirp.sync('./download');
 
-// const url = `http://download.openzim.org/release/libzim/libzim-${process.env.LIBZIM_VERSION}.tar.xz`;
-const url = `https://download.openzim.org/nightly/2019-02-06/libzim_linux-x86_64-2019-02-06.tar.gz`;
+const urls = [
+    `http://download.openzim.org/release/libzim/libzim-${process.env.LIBZIM_VERSION}.tar.xz`, // Headers
+    `http://download.openzim.org/nightly/2019-02-07/libzim_linux-x86_64-2019-02-07.tar.gz`, // Binary
+];
 
-console.info(`Downloading Libzim Binaries from: `, url);
-const dlFile = `./download/libzim-${process.env.LIBZIM_VERSION}.tar.gz`;
+for (let url of urls) {
+    console.info(`Downloading Libzim from: `, url);
+    const filename = urlParser.parse(url).pathname.split('/').slice(-1)[0];
+    const dlFile = `./download/${filename}`;
 
-try {
-    fs.statSync(dlFile);
-    console.warn(`File [${dlFile}] already exists, not downloading`);
-    process.exit(0);
-} catch (err) { }
+    try {
+        fs.statSync(dlFile);
+        console.warn(`File [${dlFile}] already exists, not downloading`);
+        return;
+    } catch (err) { }
 
-axios({
-    url,
-    method: 'get',
-    responseType: 'stream'
-})
-    .then(function (response) {
-        const ws = fs.createWriteStream(dlFile);
-        return new Promise((resolve, reject) => {
-            response.data
-                .pipe(ws)
-                .on('error', reject)
-                .on('close', resolve);
+    axios({
+        url,
+        method: 'get',
+        responseType: 'stream'
+    })
+        .then(function (response) {
+            const ws = fs.createWriteStream(dlFile);
+            return new Promise((resolve, reject) => {
+                response.data
+                    .pipe(ws)
+                    .on('error', reject)
+                    .on('close', resolve);
+            });
+        })
+        .then(() => {
+            const cmd = `tar --strip-components 1 -xf ${dlFile} -C ./download`;
+            console.log(`Running Extract:`, `[${cmd}]`);
+            return exec(cmd);
+        })
+        .then(() => {
+            console.info(`Successfully downloaded and extracted file`);
+        })
+        .catch(err => {
+            console.error(`Failed to download and extract file:`, err);
         });
-    })
-    .then(() => {
-        const cmd = `tar --strip-components 1 -xf ${dlFile} -C ./download`;
-        console.log(`Running Extract:`, `[${cmd}]`);
-        return exec(cmd);
-    })
-    .then(() => {
-        console.info(`Successfully downloaded and extracted file`);
-    })
-    .catch(err => {
-        console.error(`Failed to download and extract file:`, err);
-    });
+}
