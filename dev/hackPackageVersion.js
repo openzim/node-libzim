29ac7e4e17d9d1bc28b3bacd0a4032364a650e98
Emmanuel Engelhart <kelson@kiwix.org>
2023-12-09 18:31:03 +0100

    Remove an unused variable

--- dev/hackPackageVersion.js
+++ dev/hackPackageVersion.js
@@ -1,5 +1,4 @@
 import { writeFileSync } from "fs";
-
 import pkg, { version } from "../package.json";
-version = `${version}-${Date.now()}`;
+
 writeFileSync("./package.json", JSON.stringify(pkg), "utf8");
